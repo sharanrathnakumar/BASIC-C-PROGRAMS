f68421dcbe1f4e9f6a45888c1030b58dfe61f75e
Sharan Rathnakumar <53375728+sharanrathnakumar@users.noreply.github.com>
2022-11-24 14:40:05 +0530

    Create IsLower.c

---
+++ IsLower.c
@@ -0,0 +1,45 @@
+/*
+Name: Sharan Rathnakumar
+Date: 2nd November 2022
+Description: WAP to implement your own isxdigit() function
+Input: Enter the character: a
+Output: The character 'a' is an hexadecimal digit.
+ */
+
+#include<stdio.h>
+
+int my_islower(int);    //Initialising function
+
+
+int my_islower( int character)  //Function definition
+{
+    if( character >= 97 && character <= 122 )//checking whether character is in ascii range for alnum
+    {
+        return 1;       //When condition is true returns 1 to the function call
+    }
+    else
+    {
+        return 0;       //When condition is false returns 0 to the function call
+    }
+}
+
+int main()
+{
+    char ch;    //Initialisng character variable
+    int ret;    //Initialisinh ret variable
+
+    printf("Enter the character:");     //Display information for user
+    scanf("%c", &ch);                   //Read character to ch variable
+
+    ret = my_islower(ch);
+    if(ret)                             //Checks true if ret is 1
+    {
+        printf("Entered character is lower case alphabet\n");       //If alnum prints is alnum message
+    }
+    else                                //Else if condition is false, ret value is 0
+    {
+        printf("Entered character is not lower case alphabet\n");   //Not alnum message is printed
+    }
+
+    return 0;
+}
